fbb847b057d76cbeec378182535a222e4d9e8def
Adewumi-Opeyemi-Mathew <adewumiopeyemi001@yahoo.com>
2024-09-07 12:41:40 -0900

    updateprofile, myprofile, uploadProfilePic

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -7,6 +7,7 @@ import emailSenderTemplate from "../middlewares/email.js";
 import ejs from 'ejs';
 import jwt from 'jsonwebtoken';
 import { checkExistingPassword, checkExistingUser } from "../middlewares/services.js";
+import cloudinary from "../public/images/cloudinary.js";
 
 export const register = async (req, res) => {
   try {
@@ -59,7 +60,9 @@ export const register = async (req, res) => {
 
     return successResMsg(res, 201, {
       success: true,
-      newUser: newUser,
+      newUser: {
+        email: newUser.email
+      },
       message: 'User created successfully. Please check your email to verify your account.',
     });
   } catch (error) {
@@ -95,8 +98,7 @@ export const login = async(req, res) => {
     return successResMsg(res, 200, {
       success: true,
       data: {
-        token,
-        // role: user.role,
+        token
       },
     });
 
@@ -124,6 +126,70 @@ export const verifyEmail = async(req, res) => {
         message: 'Email verified successfully',
       });
     
+  } catch (error) {
+    console.error(error);
+    return errorResMsg(res, 500, "Server Error");
+    
+  }
+};
+
+export const getProfile = async(req, res) => {
+  try {
+    const user = req.user;
+    console.log(user);
+    
+    if(!user) {
+        return errorResMsg(res, 401, 'User not found');
+    }
+    const userProfile = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+      city: user.city,
+      address: user.address,
+      registrationDate: user.registrationDate,
+      profilePicture: user.profilePicture
+    }
+    return successResMsg(res, 200, {
+        success: true,
+        user: userProfile,
+      });
+    
+  } catch (error) {
+    console.error(error);
+    return errorResMsg(res, 500, "Server Error");
+    
+  }
+};
+
+export const updateProfile = async(req, res) => {
+  try {
+    const user = req.user;
+    const { firstName, lastName, phoneNumber, city, address } = req.body;
+    const profilePicture = req.file;
+    if(!firstName ||!lastName || !phoneNumber || !city || !address) {
+        return errorResMsg(res, 400, 'Please fill in the first name, last name, phone number, city and address');
+    }
+    if(!profilePicture) {
+      return errorResMsg(res, 400, 'Please upload a profile picture');
+    }
+    const result = await cloudinary.v2.uploader.upload(profilePicture.path);
+    const updatedUser = await User.findByIdAndUpdate(user._id, {firstName, lastName, phoneNumber, city, address, profilePicture: result.secure_url}, {new: true});
+    if(!updatedUser) {
+        return errorResMsg(res, 400, 'User not found');
+    }
+    const filteredUser = {
+      firstName: updatedUser.firstName,
+      lastName: updatedUser.lastName,
+      phoneNumber: updatedUser.phoneNumber,
+      city: updatedUser.city,
+      address: updatedUser.address,
+      profilePicture: updatedUser.profilePicture
+    }
+    return successResMsg(res, 200, {
+        success: true,
+        user: filteredUser,
+      });
   } catch (error) {
     console.error(error);
     return errorResMsg(res, 500, "Server Error");
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import {register, login, verifyEmail} from '../controllers/users.controller.js'
+import {register, login, verifyEmail, getProfile, updateProfile} from '../controllers/users.controller.js';
+import { authenticateUser } from '../middlewares/auth.js';
+import upload from "../public/images/multer.js"
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/verify-email', verifyEmail);
+router.get('/myprofile', authenticateUser, getProfile);
+router.put('/updateprofile', authenticateUser, upload.single("profilePicture"), updateProfile);
 
 
 export default router;
