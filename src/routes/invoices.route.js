fb6cfa46609e994a973a5b637b6ba16597f6ba62
Adewumi-Opeyemi-Mathew <adewumiopeyemi001@yahoo.com>
2024-10-04 12:25:24 -0900

    getInvoice

--- src/routes/invoices.route.js
+++ src/routes/invoices.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createInvoice } from '../controllers/invoices.controller.js';
+import { createInvoice, filterByStatus, getAllInvoices, getInvoiceById } from '../controllers/invoices.controller.js';
 import { authenticateUser } from '../middlewares/auth.js';
 const Router = express.Router();
 
-Router.post('/createinvoice', authenticateUser, createInvoice)
+Router.post('/createinvoice', authenticateUser, createInvoice);
+Router.get('/getinvoice/:invoiceId', authenticateUser, getInvoiceById);
+Router.get('/getallinvoice', authenticateUser, getAllInvoices);
+Router.get('/filterbystatus', authenticateUser, filterByStatus);
 
 
 export default Router;
